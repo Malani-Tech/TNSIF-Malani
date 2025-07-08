20031721099eb3cf600c92d9e0303e9a4d579cbe
user <user@H-Sys45>
2025-07-08 12:27:38 +0530

    package commit

---
+++ java_project/src/com/tnsif/packagesandaccessmodifiers/PackageExample.java
@@ -0,0 +1,15 @@
+package com.tnsif.packagesandaccessmodifiers;
+
+public class PackageExample {
+
+	public static void main(String[] args) {
+		System.out.println("Displaying PackageExample class");
+
+	}
+
+	public void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
